7a3fc18a5791cb0dec87557b2ffc776cab2003d5
dmkits <dmk.dp.ua@gmail.com>
2019-12-05 09:31:59 +0200

    Change cordova-build instruction.

--- cordova-build.sh
+++ cordova-build.sh
@@ -2,16 +2,20 @@
 export ANDROID_HOME=/home/dmkits/Android/Sdk
 export PATH=$PATH:$ANDROID_HOME/tools
 
-# for run android studio AVD and  run Android Emulator
-sudo chmod a=rw /dev/kvm
-
 # cordova build
 # OR
 # cordova build android --release -- --gradleArg=-PcdvMinSdkVersion=19
 cordova build
 
+# AndroidStudio
+# for run android studio AVD and  run Android Emulator
+sudo chmod a=rw /dev/kvm
+# run AndroidStudio
 /home/dmkits/android-studio/bin/studio.sh
-# OR
+
+# OR if run background use:
+nohup /home/dmkits/android-studio/bin/studio.sh > /dev/null 2>&1 &
+
 # AndroidStudio > Tools > AVD Manager
 # and start Your Virtual Device (Nexus 5X API 29 x86)
 
